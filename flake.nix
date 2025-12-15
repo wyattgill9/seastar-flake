c0ff36194a2fc7e6ab15fb5798a285ec6fd8fc1b
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-12-15 14:38:29 -0800

    .

--- flake.nix
+++ flake.nix
@@ -8,12 +8,12 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux = {
       default = self.packages.x86_64-linux.seastar;
-      seastar = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
+      seastar = nixpkgs.legacyPackages.x86_64-linux.callPackage ./seastar.nix {};
     };
     
     overlays.default = final: prev: {
       seastar = if prev.stdenv.isLinux && prev.stdenv.isx86_64
-        then prev.callPackage ./default.nix {}
+        then prev.callPackage ./seastar.nix {}
         else throw "Seastar is only supported on x86_64-linux";
     };
   };
